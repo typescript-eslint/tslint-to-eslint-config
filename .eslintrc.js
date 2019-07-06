7a1ddfe4215b5ebd93e1b3ea31de1a28d824e35c
Josh Goldberg <joshuakgoldberg@outlook.com>
2019-07-06 14:17:35 -0500

    Extended .eslintrc.js from typescript-eslint/all, not /recommended (#92)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,32 @@ module.exports = {
         es6: true,
         node: true,
     },
-    extends: ["airbnb-base", "eslint-config-prettier", "plugin:@typescript-eslint/recommended"],
+    extends: ["airbnb-base", "eslint-config-prettier", "plugin:@typescript-eslint/all"],
+    overrides: [
+        {
+            files: ["*.stubs.ts", "*.test.ts"],
+            rules: {
+                "@typescript-eslint/promise-function-async": 0,
+            },
+        },
+    ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: "tsconfig.json",
     },
     plugins: ["@typescript-eslint"],
     rules: {
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
         "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/generic-type-naming": 0,
         "@typescript-eslint/indent": 0,
         "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-extra-parens": 0,
+        "@typescript-eslint/no-magic-numbers": 0,
         "@typescript-eslint/no-parameter-properties": 0,
+        "@typescript-eslint/no-type-alias": 0,
         "@typescript-eslint/no-use-before-define": 0,
+        "@typescript-eslint/prefer-for-of": 1,
         "@typescript-eslint/prefer-interface": 0,
         "default-case": 0,
         "guard-for-in": 0,
