fd7b813569e17e3799ff936bffaf5fd3c674a763
Josh Goldberg <josh@fullscreenmario.com>
2019-07-30 06:11:46 -0400

    Added options for CLI flags to end-to-end tests

--- test/createTestArgs.ts
+++ test/createTestArgs.ts
@@ -4,7 +4,7 @@ import { promisify } from "util";
 
 const readdir = promisify(fs.readdir);
 
-export const createTestArgs = async (cwd: string) => {
+export const createTestArgs = async (cwd: string, extraArgs: string[]) => {
     const items = new Set(await readdir(cwd));
     const flags = ["--config", path.join(cwd, ".eslintrc.json")];
 
@@ -12,5 +12,5 @@ export const createTestArgs = async (cwd: string) => {
         flags.push("--tslint", path.join(cwd, "tslint.json"));
     }
 
-    return flags.map(flag => `"${flag}"`).join(" ");
+    return [...flags.map(flag => `"${flag}"`).join(" "), ...extraArgs];
 };
