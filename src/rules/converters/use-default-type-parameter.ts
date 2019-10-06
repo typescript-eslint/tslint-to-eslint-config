056d18d2ec7c8ea01a8ecc7e27773e5cafefe758
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-10-06 14:01:18 -0700

    code review changes

--- src/rules/converters/use-default-type-parameter.ts
+++ src/rules/converters/use-default-type-parameter.ts
@@ -1,7 +1,5 @@
 import { RuleConverter } from "../converter";
 
-// See here for reference
-// https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unnecessary-type-arguments.md
 export const convertUseDefaultTypeParameter: RuleConverter = () => {
     return {
         rules: [
