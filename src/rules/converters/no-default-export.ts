c84f8ec85841eb36fd79d8a44489a1850aa44a00
david.wuerfel <david.wuerfel@centigrade.de>
2019-10-06 12:53:58 +0200

    feat (converter): Use eslint import plugin for no-default-export

--- src/rules/converters/no-default-export.ts
+++ src/rules/converters/no-default-export.ts
@@ -3,7 +3,10 @@ import { RuleConverter } from "../converter";
 export const convertNoDefaultExport: RuleConverter = () => {
     return {
         rules: [
-            // TODO: devinmotion: Using import/no-default-export
+            {
+                ruleName: "import/no-default-export",
+            },
         ],
+        plugins: ["import"],
     };
 };
