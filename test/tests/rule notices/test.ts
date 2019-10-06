764498dcdab42dc4c41b564f7a659a3af1832195
Josh Goldberg <josh@fullscreenmario.com>
2019-10-06 17:39:48 -0400

    Removed unnecessary new test options

--- test/tests/rule notices/test.ts
+++ test/tests/rule notices/test.ts
@@ -1,8 +1,3 @@
 import { createTests } from "../../createTests";
 
-const eslint = "./eslintrc.js";
-
-createTests(__dirname, {
-    eslint,
-    extraArgs: ["--eslint", eslint],
-});
+createTests(__dirname);
