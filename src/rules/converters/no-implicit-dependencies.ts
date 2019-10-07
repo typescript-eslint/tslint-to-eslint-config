e54c4bd5558aa57ffc01fb96de8c1b38fbf51b14
melbarch <elbarchany@gmail.com>
2019-10-07 21:16:20 +0100

    Add no-implicit-dependencies converter

---
+++ src/rules/converters/no-implicit-dependencies.ts
@@ -0,0 +1,21 @@
+import { RuleConverter } from "../converter";
+
+export const convertNoImplicitDependencies: RuleConverter = tslintRule => {
+    const ruleArgs = [];
+
+    tslintRule.ruleArguments.forEach(element => {
+        if (element === "dev") {
+            ruleArgs.push({ devDependencies: false });
+        } else if (element === "optional") {
+            ruleArgs.push({ optionalDependencies: false });
+        }
+    });
+    return {
+        rules: [
+            {
+                ruleName: "import/no-extraneous-dependencies",
+                ...(ruleArgs.length > 0 && { ruleArguments: ruleArgs }),
+            },
+        ],
+    };
+};
---
+++ src/rules/converters/tests/no-implicit-dependencies.test.ts
@@ -0,0 +1,62 @@
+import { convertNoImplicitDependencies } from "../no-implicit-dependencies";
+
+describe(convertNoImplicitDependencies, () => {
+    test("conversion without arguments", () => {
+        const result = convertNoImplicitDependencies({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "import/no-extraneous-dependencies",
+                },
+            ],
+        });
+    });
+
+    test("conversion with dev argument", () => {
+        const result = convertNoImplicitDependencies({
+            ruleArguments: ["dev"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [{ devDependencies: false }],
+                    ruleName: "import/no-extraneous-dependencies",
+                },
+            ],
+        });
+    });
+
+    test("conversion with optional argument", () => {
+        const result = convertNoImplicitDependencies({
+            ruleArguments: ["optional"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [{ optionalDependencies: false }],
+                    ruleName: "import/no-extraneous-dependencies",
+                },
+            ],
+        });
+    });
+
+    test("conversion with both dev and optional argument", () => {
+        const result = convertNoImplicitDependencies({
+            ruleArguments: ["dev", "optional"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [{ devDependencies: false }, { optionalDependencies: false }],
+                    ruleName: "import/no-extraneous-dependencies",
+                },
+            ],
+        });
+    });
+});
