ea5832031382742b548b0b3998cd0ef66d76aa8c
Jeremy Yap <dcsyjlj@nus.edu.sg>
2019-10-07 13:43:12 +0800

    Add no-caller and no-eval mergers

--- src/rules/mergers.ts
+++ src/rules/mergers.ts
@@ -1,7 +1,11 @@
 import { mergeBanTypes } from "./mergers/ban-types";
+import { mergeNoCaller } from "./mergers/no-caller";
+import { mergeNoEval } from "./mergers/no-eval";
 import { mergeNoUnnecessaryTypeAssertion } from "./mergers/no-unnecessary-type-assertion";
 
 export const mergers = new Map([
     ["@typescript-eslint/ban-types", mergeBanTypes],
     ["@typescript-eslint/no-unnecessary-type-assertion", mergeNoUnnecessaryTypeAssertion],
+    ["no-caller", mergeNoCaller],
+    ["no-eval", mergeNoEval],
 ]);
