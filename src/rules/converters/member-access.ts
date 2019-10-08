cb72bb218bea131ebb5d6404986d52daa14a104d
KingDarBoja <mbojato@uninorte.edu.co>
2019-10-07 20:53:58 -0500

    Handle rule options on member access tslint

--- src/rules/converters/member-access.ts
+++ src/rules/converters/member-access.ts
@@ -1,15 +1,67 @@
 import { RuleConverter } from "../converter";
 
-export const convertMemberAccess: RuleConverter = () => {
+export enum AccessibilityLevel {
+    Explicit = "explicit", // require an accessor (including public)
+    NoPublic = "no-public", // don't require public
+    Off = "off", // don't check
+}
+
+export enum MemberAccessArguments {
+    NoPublic = "no-public",
+    Accessor = "check-accessor",
+    Constructor = "check-constructor",
+    ParameterProp = "check-parameter-property",
+}
+
+export const convertMemberAccess: RuleConverter = tslintRule => {
+    const schema: any = {
+        accessibility: AccessibilityLevel.Explicit,
+    };
+
+    if (
+        !(
+            tslintRule.ruleArguments.length === 0 ||
+            tslintRule.ruleArguments[0] === false ||
+            tslintRule.ruleArguments.length < 2
+        )
+    ) {
+        tslintRule.ruleArguments.map(ruleArg => {
+            if (typeof ruleArg === "string") {
+                switch (ruleArg) {
+                    case MemberAccessArguments.NoPublic:
+                        schema.accessibility = AccessibilityLevel.NoPublic;
+                        break;
+                    case MemberAccessArguments.Accessor:
+                        schema.overrides = {
+                            ...schema.overrides,
+                            accessors: AccessibilityLevel.Explicit,
+                        };
+                        break;
+                    case MemberAccessArguments.Constructor:
+                        schema.overrides = {
+                            ...schema.overrides,
+                            constructors: AccessibilityLevel.Explicit,
+                        };
+                        break;
+                    case MemberAccessArguments.ParameterProp:
+                        schema.overrides = {
+                            ...schema.overrides,
+                            parameterProperties: AccessibilityLevel.Explicit,
+                        };
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return ruleArg;
+        });
+    }
+
     return {
         rules: [
             {
                 ruleName: "@typescript-eslint/explicit-member-accessibility",
-                ruleArguments: [
-                    {
-                        overrides: { constructors: "off" },
-                    },
-                ],
+                ruleArguments: [schema],
             },
         ],
     };
--- src/rules/converters/tests/member-access.test.ts
+++ src/rules/converters/tests/member-access.test.ts
@@ -1,4 +1,4 @@
-import { convertMemberAccess } from "../member-access";
+import { convertMemberAccess, AccessibilityLevel } from "../member-access";
 
 describe(convertMemberAccess, () => {
     test("conversion without arguments", () => {
@@ -10,8 +10,117 @@ describe(convertMemberAccess, () => {
             rules: [
                 {
                     ruleName: "@typescript-eslint/explicit-member-accessibility",
+                    ruleArguments: [{ accessibility: AccessibilityLevel.Explicit }],
+                },
+            ],
+        });
+    });
+
+    test("conversion with no-public argument", () => {
+        const result = convertMemberAccess({
+            ruleArguments: [true, "no-public"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/explicit-member-accessibility",
+                    ruleArguments: [{ accessibility: AccessibilityLevel.NoPublic }],
+                },
+            ],
+        });
+    });
+
+    test("conversion with check-accessor argument", () => {
+        const result = convertMemberAccess({
+            ruleArguments: [true, "check-accessor"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/explicit-member-accessibility",
                     ruleArguments: [
-                        { overrides: { constructors: "off" } },
+                        {
+                            accessibility: AccessibilityLevel.Explicit,
+                            overrides: {
+                                accessors: AccessibilityLevel.Explicit,
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+
+    test("conversion with check-constructor argument", () => {
+        const result = convertMemberAccess({
+            ruleArguments: [true, "check-constructor"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/explicit-member-accessibility",
+                    ruleArguments: [
+                        {
+                            accessibility: AccessibilityLevel.Explicit,
+                            overrides: {
+                                constructors: AccessibilityLevel.Explicit,
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+
+    test("conversion with check-parameter-property argument", () => {
+        const result = convertMemberAccess({
+            ruleArguments: [true, "check-parameter-property"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/explicit-member-accessibility",
+                    ruleArguments: [
+                        {
+                            accessibility: AccessibilityLevel.Explicit,
+                            overrides: {
+                                parameterProperties: AccessibilityLevel.Explicit,
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+
+    test("conversion with all arguments", () => {
+        const result = convertMemberAccess({
+            ruleArguments: [
+                true,
+                "no-public",
+                "check-accessor",
+                "check-constructor",
+                "check-parameter-property",
+            ],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/explicit-member-accessibility",
+                    ruleArguments: [
+                        {
+                            accessibility: AccessibilityLevel.NoPublic,
+                            overrides: {
+                                accessors: AccessibilityLevel.Explicit,
+                                constructors: AccessibilityLevel.Explicit,
+                                parameterProperties: AccessibilityLevel.Explicit,
+                            },
+                        },
                     ],
                 },
             ],
