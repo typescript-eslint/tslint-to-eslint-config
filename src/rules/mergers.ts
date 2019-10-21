13ea4746f67b30057105f01f57dd5e8939d5ca6d
Jeremy Yap <dcsyjlj@nus.edu.sg>
2019-10-21 16:21:25 +0800

    Add merger for indent rule

--- src/rules/mergers.ts
+++ src/rules/mergers.ts
@@ -1,10 +1,12 @@
 import { mergeBanTypes } from "./mergers/ban-types";
+import { mergeIndent } from "./mergers/indent";
 import { mergeNoCaller } from "./mergers/no-caller";
 import { mergeNoEval } from "./mergers/no-eval";
 import { mergeNoUnnecessaryTypeAssertion } from "./mergers/no-unnecessary-type-assertion";
 
 export const mergers = new Map([
     ["@typescript-eslint/ban-types", mergeBanTypes],
+    ["@typescript-eslint/indent", mergeIndent],
     ["@typescript-eslint/no-unnecessary-type-assertion", mergeNoUnnecessaryTypeAssertion],
     ["no-caller", mergeNoCaller],
     ["no-eval", mergeNoEval],
