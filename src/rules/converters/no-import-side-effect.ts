5709e83f45b7621188c7e9c39cddb6af0ff30565
Brandon Everett <bme2010@gmail.com>
2019-10-21 06:36:04 -0400

    only show notice for manually converting rule

--- src/rules/converters/no-import-side-effect.ts
+++ src/rules/converters/no-import-side-effect.ts
@@ -1,11 +1,9 @@
 import { RuleConverter } from "../converter";
 
 export const convertNoImportSideEffect: RuleConverter = tsLintRule => {
-    const rules = [];
     const notices = [];
 
     if (tsLintRule.ruleArguments.length > 0) {
-        rules.push({ allow: tsLintRule.ruleArguments[1]["ignore-module"] });
         notices.push(
             "ESLint's no-import-side-effect now accepts a glob pattern for ignores; you'll need to manually convert your ignore-module settings.",
         );
@@ -14,7 +12,7 @@ export const convertNoImportSideEffect: RuleConverter = tsLintRule => {
     return {
         rules: [
             {
-                ruleArguments: rules,
+                ruleArguments: [],
                 ruleName: "no-import-side-effect",
                 notices: notices,
             },
--- src/rules/converters/tests/no-import-side-effect.test.ts
+++ src/rules/converters/tests/no-import-side-effect.test.ts
@@ -25,7 +25,7 @@ describe(convertNoImportSideEffect, () => {
         expect(result).toEqual({
             rules: [
                 {
-                    ruleArguments: [{ allow: "(\\.html|\\.css)$" }],
+                    ruleArguments: [],
                     ruleName: "no-import-side-effect",
                     notices: [
                         "ESLint's no-import-side-effect now accepts a glob pattern for ignores; you'll need to manually convert your ignore-module settings.",
