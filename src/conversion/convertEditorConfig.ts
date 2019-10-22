da92c2be742f943969e7c119c1067157df952da0
David Würfel <david.wuerfel@googlemail.com>
2019-10-22 20:58:21 +0200

    style: format with prettier

--- src/conversion/convertEditorConfig.ts
+++ src/conversion/convertEditorConfig.ts
@@ -1,11 +1,11 @@
 import { FileSystem } from "../adapters/fileSystem";
 import { SansDependencies } from "../binding";
-import {
-    findEditorConfiguration,
-    DEFAULT_VSCODE_SETTINGS_PATH,
-} from "../input/findEditorConfiguration";
-import { TSLintToESLintSettings, ResultWithStatus, ResultStatus } from "../types";
 import { formatOutput } from "../creation/formatting/formatOutput";
+import {
+    DEFAULT_VSCODE_SETTINGS_PATH,
+    findEditorConfiguration,
+} from "../input/findEditorConfiguration";
+import { ResultStatus, ResultWithStatus, TSLintToESLintSettings } from "../types";
 
 export type ConvertEditorConfigDependencies = {
     findEditorConfiguration: SansDependencies<typeof findEditorConfiguration>;
@@ -45,32 +45,29 @@ export const convertEditorConfig = async (
         ...codeActionsOnSaveWithoutReplacedProperties
     } = codeActionsOnSave;
 
-    // Nothing to convert
-    if (!originalSourceFixAllTsLint) {
-        return {
-            status: ResultStatus.Succeeded,
+    // Something to convert
+    if (originalSourceFixAllTsLint) {
+        // 4. Rewrite editor configuration with replacements.
+        const newEditorConfiguration = {
+            ...originalEditorConfiguration,
+            "editor.codeActionsOnSave": codeActionsOnSaveWithoutReplacedProperties,
+            "eslint.autoFixOnSave": originalSourceFixAllTsLint,
         };
-    }
 
-    // 4. Rewrite editor configuration with replacements.
-    const newEditorConfiguration = {
-        ...originalEditorConfiguration,
-        "editor.codeActionsOnSave": codeActionsOnSaveWithoutReplacedProperties,
-        "eslint.autoFixOnSave": originalSourceFixAllTsLint,
-    };
+        const output = newEditorConfiguration;
+        const outputPath = settings.editor ? settings.editor : DEFAULT_VSCODE_SETTINGS_PATH;
 
-    const output = newEditorConfiguration;
-    const outputPath = settings.editor ? settings.editor : DEFAULT_VSCODE_SETTINGS_PATH;
+        const fileWriteError = await dependencies.fileSystem.writeFile(
+            outputPath,
+            formatOutput(outputPath, output),
+        );
 
-    const fileWriteError = await dependencies.fileSystem.writeFile(
-        outputPath,
-        formatOutput(outputPath, output),
-    );
-    if (fileWriteError !== undefined) {
-        return {
-            errors: [fileWriteError],
-            status: ResultStatus.Failed,
-        };
+        if (fileWriteError !== undefined) {
+            return {
+                errors: [fileWriteError],
+                status: ResultStatus.Failed,
+            };
+        }
     }
 
     return {
