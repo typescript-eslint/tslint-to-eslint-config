35970790d645648bae85c7bcec731d368d780672
Dmytro Borysovskyi <bbbara10@gmail.com>
2019-11-26 20:26:45 +0100

    fix: generating `space-before-function-paren` (#147)

--- src/rules/converters/space-before-function-paren.ts
+++ src/rules/converters/space-before-function-paren.ts
@@ -1,11 +1,26 @@
 import { RuleConverter } from "../converter";
 
+const NOT_SUPPORTED_OPTIONS = ["constructor", "method"];
+
+function isExistedESLintOption(rule: string) {
+    return !NOT_SUPPORTED_OPTIONS.includes(rule);
+}
+
 export const convertSpaceBeforeFunctionParen: RuleConverter = tslintRule => {
+    const ruleArguments = tslintRule.ruleArguments.filter(isExistedESLintOption);
+    const notices = NOT_SUPPORTED_OPTIONS.reduce<string[]>((acc, option) => {
+        if (tslintRule.ruleArguments.includes(option)) {
+            acc.push(`Option "${option}" is not supported by ESLint.`);
+        }
+        return acc;
+    }, []);
+
     return {
         rules: [
             {
                 ...(tslintRule.ruleArguments.length !== 0 && {
-                    ruleArguments: tslintRule.ruleArguments,
+                    ruleArguments,
+                    ...(notices.length !== 0 && { notices }),
                 }),
                 ruleName: "space-before-function-paren",
             },
