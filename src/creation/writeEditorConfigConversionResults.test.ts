7d6ce31a19067528add6c8f189e8de7d7b356bb5
David Würfel <david.wuerfel@googlemail.com>
2019-12-04 05:54:53 +0100

    test: write editor config output with sorted keys to file system

--- src/creation/writeEditorConfigConversionResults.test.ts
+++ src/creation/writeEditorConfigConversionResults.test.ts
@@ -1,6 +1,10 @@
 import { createStubFileSystem } from "../adapters/fileSystem.stub";
 import { createEmptySettingConversionResults } from "../conversion/conversionResults.stubs";
+import { EditorConfiguration } from "../input/editorConfiguration";
+import { DeepPartial } from "../input/findReportedConfiguration";
+import { SettingConversionResults } from "../settings/convertSettings";
 import { EditorSetting } from "../settings/types";
+import { formatJsonOutput } from "./formatting/formatters/formatJsonOutput";
 import {
     writeConversionResults,
     WriteConversionResultsDependencies,
@@ -12,28 +16,11 @@ const createStubDependencies = (overrides: Partial<WriteConversionResultsDepende
 });
 
 describe("writeConversionResults", () => {
-    it("write with file system to correct output path", async () => {
+    it("writes to correct output path with file system", async () => {
         // Arrange
-        const originalConfig = {
-            "typescript.tsdk": "node_modules/typescript/lib",
-            "editor.tabSize": 4,
-            "editor.codeActionsOnSave": {
-                "source.organizeImports": false,
-            },
-        };
-        const outputPath = "/temp";
         const dependencies = createStubDependencies();
-        const conversionResults = createEmptySettingConversionResults({
-            converted: new Map<string, EditorSetting>([
-                [
-                    "tslint-setting-one",
-                    {
-                        settingName: "tslint-setting-one",
-                        value: 42,
-                    },
-                ],
-            ]),
-        });
+        const outputPath = "/temp";
+        const { originalConfig, conversionResults } = setupConversionEnvironment();
 
         // Act
         await writeConversionResults(dependencies, outputPath, conversionResults, originalConfig);
@@ -44,4 +31,88 @@ describe("writeConversionResults", () => {
             expect.anything(),
         );
     });
+
+    it("writes formatted output with sorted keys to file system", async () => {
+        // Arrange
+        const dependencies = createStubDependencies();
+        const outputPath = "/temp";
+
+        const { originalConfig, conversionResults } = setupConversionEnvironment({
+            originalConfig: {
+                "property.a": "someValue",
+                "property.c": 123,
+                "property.b": {
+                    "unsorted.sub.property.b": false,
+                    "unsorted.sub.property.a": false,
+                },
+            },
+            conversionResults: createEmptySettingConversionResults({
+                converted: new Map<string, EditorSetting>([
+                    [
+                        "eslint-setting-b",
+                        {
+                            settingName: "eslint-setting-b",
+                            value: 42,
+                        },
+                    ],
+                    [
+                        "eslint-setting-a",
+                        {
+                            settingName: "eslint-setting-a",
+                            value: 4711,
+                        },
+                    ],
+                ]),
+            }),
+        });
+
+        const expectedSortedOutput = formatJsonOutput({
+            "eslint-setting-a": 4711,
+            "eslint-setting-b": 42,
+            "property.a": "someValue",
+            "property.b": {
+                "unsorted.sub.property.b": false,
+                "unsorted.sub.property.a": false,
+            },
+            "property.c": 123,
+        });
+
+        // Act
+        await writeConversionResults(dependencies, outputPath, conversionResults, originalConfig);
+
+        // Assert
+        expect(dependencies.fileSystem.writeFile).toHaveBeenCalledWith(
+            expect.anything(),
+            expectedSortedOutput,
+        );
+    });
 });
+
+function setupConversionEnvironment(
+    overrides: {
+        conversionResults?: SettingConversionResults;
+        originalConfig?: DeepPartial<EditorConfiguration>;
+    } = {},
+) {
+    return {
+        originalConfig: {
+            "typescript.tsdk": "node_modules/typescript/lib",
+            "editor.tabSize": 4,
+            "editor.codeActionsOnSave": {
+                "source.organizeImports": false,
+            },
+        },
+        conversionResults: createEmptySettingConversionResults({
+            converted: new Map<string, EditorSetting>([
+                [
+                    "tslint-setting-one",
+                    {
+                        settingName: "tslint-setting-one",
+                        value: 42,
+                    },
+                ],
+            ]),
+        }),
+        ...overrides,
+    };
+}
