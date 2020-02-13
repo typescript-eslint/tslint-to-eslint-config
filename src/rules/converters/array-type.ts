1938d3017bb3ff7aadc6bf7d1e79d51370595882
Matthias Kunnen <matthias.kunnen@gmail.com>
2020-02-13 18:02:52 +0000

    Fix arguments passed to array-type

--- src/rules/converters/array-type.ts
+++ src/rules/converters/array-type.ts
@@ -1,9 +1,12 @@
 import { RuleConverter } from "../converter";
 
-export const convertArrayType: RuleConverter = () => {
+export const convertArrayType: RuleConverter = tslintRule => {
     return {
         rules: [
             {
+                ...(tslintRule.ruleArguments.length > 0 && {
+                    ruleArguments: [{ default: tslintRule.ruleArguments[0] }],
+                }),
                 ruleName: "@typescript-eslint/array-type",
             },
         ],
