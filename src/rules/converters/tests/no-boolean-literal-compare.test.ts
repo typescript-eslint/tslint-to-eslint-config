abbb708ef18201f82932e789525c82e78ea31f4c
KingDarBoja <stevenbojato04@gmail.com>
2020-02-14 22:58:08 -0500

    Add no-boolean-literal-compare converter

---
+++ src/rules/converters/tests/no-boolean-literal-compare.test.ts
@@ -0,0 +1,17 @@
+import { convertNoBooleanLiteralCompare } from "../no-boolean-literal-compare";
+
+describe(convertNoBooleanLiteralCompare, () => {
+    test("conversion without arguments", () => {
+        const result = convertNoBooleanLiteralCompare({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/no-unnecessary-boolean-literal-compare",
+                },
+            ],
+        });
+    });
+});
