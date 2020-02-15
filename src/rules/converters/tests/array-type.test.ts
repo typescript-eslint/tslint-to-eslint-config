ea960c72ad20ed1c976004d25243314f1b4df718
Matthias Kunnen <matthias.kunnen@gmail.com>
2020-02-15 04:18:51 +0100

    Fix arguments passed to array-type (#334)
    * Add test for arguments passed to array-type
    
    See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/array-type.md
    
    * Fix arguments passed to array-type

--- src/rules/converters/tests/array-type.test.ts
+++ src/rules/converters/tests/array-type.test.ts
@@ -14,4 +14,46 @@ describe(convertArrayType, () => {
             ],
         });
     });
+    test("conversion with argument array-simple", () => {
+        const result = convertArrayType({
+            ruleArguments: ["array-simple"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/array-type",
+                    ruleArguments: [{ default: "array-simple" }],
+                },
+            ],
+        });
+    });
+    test("conversion with argument generic", () => {
+        const result = convertArrayType({
+            ruleArguments: ["generic"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/array-type",
+                    ruleArguments: [{ default: "generic" }],
+                },
+            ],
+        });
+    });
+    test("conversion with argument simple", () => {
+        const result = convertArrayType({
+            ruleArguments: ["simple"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/array-type",
+                    ruleArguments: [{ default: "simple" }],
+                },
+            ],
+        });
+    });
 });
