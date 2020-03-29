3afd2d229c2b405a0a36f87feb6ffc1b2c98be01
moznion <moznion@gmail.com>
2020-03-30 01:14:21 +0900

    Respect `ban-types` configuration of TSLint
    In the past, this tool didn't respect the TSLint configuration of `ban-types`,
    it means even if TSLint specified `ban-types` with the argument, generated
    ESLint configuration doesn't contain the detailed configuration of that.
    
    For example, when the TSLint configuration is like the following:
    
    ```
    {
      "rules": {
        "ban-types": [true,
          ["Object", "Use {} instead."],
        ]
      }
    }
    ```
    
    then, older tool generates the following ESLint configuration:
    
    ```
      "rules": {
          "@typescript-eslint/ban-types": "error"
      }
    ```
    
    According to the official documentation, probably this generated result
    doesn't make sense because each rule requires the type name (and the message).
    
    https://github.com/typescript-eslint/typescript-eslint/blob/f3160b471f8247e157555b6cf5b40a1f6ccdc233/packages/eslint-plugin/docs/rules/ban-types.md
    
    So this commit makes the generated ESLint configuration to be suitable to the certainly
    working configuration, like so:
    
    ```
      "rules": {
          "@typescript-eslint/ban-types": [
              "error",
              {
                  "types": {
                      "Object": {
                          "message": "Use {} instead."
                      }
                  }
              }
          ]
      }
    ```
    
    See also: https://palantir.github.io/tslint/rules/ban-types/
    Related ticket: https://github.com/typescript-eslint/tslint-to-eslint-config/issues/352

--- src/rules/converters/ban-types.ts
+++ src/rules/converters/ban-types.ts
@@ -1,10 +1,33 @@
 import { RuleConverter } from "../converter";
 
-export const convertBanTypes: RuleConverter = () => {
+export const convertBanTypes: RuleConverter = tslintRule => {
+    type ConvertBanTypeArgument = {
+        message: string;
+    };
+
+    const bannedTypesObj: { [key: string]: ConvertBanTypeArgument | null } = {};
+
+    for (const rule of tslintRule.ruleArguments) {
+        const typ = rule[0];
+        if (!typ) {
+            break;
+        }
+
+        bannedTypesObj[typ] = rule[1]
+            ? {
+                  message: rule[1],
+              }
+            : null;
+    }
+
+    const ruleArguments =
+        Object.keys(bannedTypesObj).length === 0 ? undefined : [{ types: bannedTypesObj }];
+
     return {
         rules: [
             {
                 ruleName: "@typescript-eslint/ban-types",
+                ...{ ruleArguments },
             },
         ],
     };
--- src/rules/converters/tests/ban-types.test.ts
+++ src/rules/converters/tests/ban-types.test.ts
@@ -14,4 +14,59 @@ describe(convertBanTypes, () => {
             ],
         });
     });
+
+    test("conversion with arguments", () => {
+        const result = convertBanTypes({
+            ruleArguments: [
+                ["Object", "Use {} instead."],
+                ["String"],
+                ["Number"],
+                ["Boolean", "Use 'boolean' instead."],
+                [], // empty array: this should be ignored
+            ],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/ban-types",
+                    ruleArguments: [
+                        {
+                            types: {
+                                Object: {
+                                    message: "Use {} instead.",
+                                },
+                                String: null,
+                                Number: null,
+                                Boolean: {
+                                    message: "Use 'boolean' instead.",
+                                },
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+
+    test("conversion with duplicated arguments", () => {
+        const result = convertBanTypes({
+            ruleArguments: [["Object", "Use {} instead."], ["Object"]],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/ban-types",
+                    ruleArguments: [
+                        {
+                            types: {
+                                Object: null,
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
 });
