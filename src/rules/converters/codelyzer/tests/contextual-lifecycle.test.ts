378efff7d750e9926fc34f9496de7c68cc995ca2
KingDarBoja <stevenbojato04@gmail.com>
2020-05-08 17:48:48 -0500

    Add codelyzer contextual-lifecycle converter

---
+++ src/rules/converters/codelyzer/tests/contextual-lifecycle.test.ts
@@ -0,0 +1,18 @@
+import { convertContextualLifecycle } from "../contextual-lifecycle";
+
+describe(convertContextualLifecycle, () => {
+    test("conversion without arguments", () => {
+        const result = convertContextualLifecycle({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@angular-eslint/contextual-lifecycle",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin"],
+        });
+    });
+});
