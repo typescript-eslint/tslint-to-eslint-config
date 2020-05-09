daee71581d2516f9f4c49ba703acfc72e327d8fd
Manuel Bojato <30560560+KingDarBoja@users.noreply.github.com>
2020-05-09 17:40:17 -0500

    Add codelyzer no-conflicting-lifecycle converter (#538)

---
+++ src/rules/converters/codelyzer/tests/no-conflicting-lifecycle.test.ts
@@ -0,0 +1,18 @@
+import { convertNoConflictingLifecycle } from "../no-conflicting-lifecycle";
+
+describe(convertNoConflictingLifecycle, () => {
+    test("conversion without arguments", () => {
+        const result = convertNoConflictingLifecycle({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@angular-eslint/no-conflicting-lifecycle",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin"],
+        });
+    });
+});
