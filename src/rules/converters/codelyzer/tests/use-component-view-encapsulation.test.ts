1a8f5e775d93299b690ad1872d9019eeeecc0e0b
Josh Goldberg <me@joshuakgoldberg.com>
2020-05-09 19:49:27 -0400

    Added codelyzr use-component-view-encapsulation converter
    Another non-configurable rule. ⚡

---
+++ src/rules/converters/codelyzer/tests/use-component-view-encapsulation.test.ts
@@ -0,0 +1,18 @@
+import { convertUseComponentViewEncapsulation } from "../use-component-view-encapsulation";
+
+describe(convertUseComponentViewEncapsulation, () => {
+    test("conversion without arguments", () => {
+        const result = convertUseComponentViewEncapsulation({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@angular-eslint/use-component-view-encapsulation",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin"],
+        });
+    });
+});
