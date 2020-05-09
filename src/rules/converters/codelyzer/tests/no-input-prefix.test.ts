1410dd39cf9f7afed531e43031efbf4b473c7f69
Manuel Bojato <30560560+KingDarBoja@users.noreply.github.com>
2020-05-09 16:31:13 -0500

    Add codelyzer no-input-prefix converter (#531)

---
+++ src/rules/converters/codelyzer/tests/no-input-prefix.test.ts
@@ -0,0 +1,38 @@
+import { convertNoInputPrefix } from "../no-input-prefix";
+
+describe(convertNoInputPrefix, () => {
+    test("conversion without arguments", () => {
+        const result = convertNoInputPrefix({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@angular-eslint/no-input-prefix",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin"],
+        });
+    });
+
+    test("conversion with arguments", () => {
+        const result = convertNoInputPrefix({
+            ruleArguments: ["can", "is", "should"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [
+                        {
+                            prefixes: ["can", "is", "should"],
+                        },
+                    ],
+                    ruleName: "@angular-eslint/no-input-prefix",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin"],
+        });
+    });
+});
