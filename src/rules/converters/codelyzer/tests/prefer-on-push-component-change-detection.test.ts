1c05e47c5790cd4a62df2a03a7f86b8e7afc4049
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-05-10 14:19:49 -0400

    Added Codelyzer prefer-on-push-component-change-detection converter (#571)
    Another non-configurable rule. ⚡
    
    http://codelyzer.com/rules/prefer-on-push-component-change-detection / https://github.com/angular-eslint/angular-eslint/blob/master/packages/eslint-plugin/src/rules/prefer-on-push-component-change-detection.ts

---
+++ src/rules/converters/codelyzer/tests/prefer-on-push-component-change-detection.test.ts
@@ -0,0 +1,18 @@
+import { convertPreferOnPushComponentChangeDetection } from "../prefer-on-push-component-change-detection";
+
+describe(convertPreferOnPushComponentChangeDetection, () => {
+    test("conversion without arguments", () => {
+        const result = convertPreferOnPushComponentChangeDetection({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@angular-eslint/prefer-on-push-component-change-detection",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin"],
+        });
+    });
+});
