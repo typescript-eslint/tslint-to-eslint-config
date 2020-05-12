0b1cab4a89b394a17c2da755a8fda0169790bc85
Bill Barry <after.fallout@gmail.com>
2020-05-11 20:26:10 -0400

    Update template-cyclomatic-complexity.test.ts

--- src/rules/converters/codelyzer/tests/template-cyclomatic-complexity.test.ts
+++ src/rules/converters/codelyzer/tests/template-cyclomatic-complexity.test.ts
@@ -15,4 +15,24 @@ describe(convertTemplateCyclomaticComplexity, () => {
             plugins: ["@angular-eslint/eslint-plugin-template"],
         });
     });
+    
+    test("conversion with arguments", () => {
+        const result = convertTemplateCyclomaticComplexity({
+            ruleArguments: [4],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [
+                        {
+                            maxComplexity: 4,
+                        },
+                    ],
+                    ruleName: "@angular-eslint/template/cyclomatic-complexity",
+                },
+            ],
+            plugins: ["@angular-eslint/eslint-plugin-template"],
+        });
+    });
 });
