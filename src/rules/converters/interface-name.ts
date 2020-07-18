770e0275892e09c8a8d0edf2ae532ba001d3abfd
KingDarBoja <stevenbojato04@gmail.com>
2020-07-17 20:02:47 -0500

    Fix interface-name converter

--- src/rules/converters/interface-name.ts
+++ src/rules/converters/interface-name.ts
@@ -1,10 +1,23 @@
 import { RuleConverter } from "../converter";
 
-export const convertInterfaceName: RuleConverter = () => {
+export const convertInterfaceName: RuleConverter = (tslintRule) => {
     return {
         rules: [
             {
                 ruleName: "@typescript-eslint/naming-convention",
+                ...(tslintRule.ruleArguments.length !== 0 && {
+                    rules: [
+                        {
+                            selector: "interface",
+                            format: ["PascalCase"],
+                            custom: {
+                                regex: "^I[A-Z]",
+                                match:
+                                    tslintRule.ruleArguments[0] === "always-prefix" ? true : false,
+                            },
+                        },
+                    ],
+                }),
             },
         ],
     };
--- src/rules/converters/tests/interface-name.test.ts
+++ src/rules/converters/tests/interface-name.test.ts
@@ -14,4 +14,52 @@ describe(convertInterfaceName, () => {
             ],
         });
     });
+
+    test("conversion with 'always-prefix' argument", () => {
+        const result = convertInterfaceName({
+            ruleArguments: ["always-prefix"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/naming-convention",
+                    rules: [
+                        {
+                            selector: "interface",
+                            format: ["PascalCase"],
+                            custom: {
+                                regex: "^I[A-Z]",
+                                match: true,
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+
+    test("conversion with 'never-prefix' argument", () => {
+        const result = convertInterfaceName({
+            ruleArguments: ["never-prefix"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/naming-convention",
+                    rules: [
+                        {
+                            selector: "interface",
+                            format: ["PascalCase"],
+                            custom: {
+                                regex: "^I[A-Z]",
+                                match: false,
+                            },
+                        },
+                    ],
+                },
+            ],
+        });
+    });
 });
