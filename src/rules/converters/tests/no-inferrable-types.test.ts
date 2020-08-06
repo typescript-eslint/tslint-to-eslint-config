040b4387941bb7a27bfa8c6d1db46953452aaea1
Miho Maehara <mihom28jun@gmail.com>
2020-08-06 15:38:22 -0700

    fix: Convert options for no-inferrable-types (#669)

--- src/rules/converters/tests/no-inferrable-types.test.ts
+++ src/rules/converters/tests/no-inferrable-types.test.ts
@@ -14,4 +14,34 @@ describe(convertNoInferrableTypes, () => {
             ],
         });
     });
+
+    test("conversion with an argument", () => {
+        const result = convertNoInferrableTypes({
+            ruleArguments: ["ignore-params"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/no-inferrable-types",
+                    ruleArguments: [{ ignoreParameters: true }],
+                },
+            ],
+        });
+    });
+
+    test("conversion with arguments", () => {
+        const result = convertNoInferrableTypes({
+            ruleArguments: ["ignore-params", "ignore-properties"],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "@typescript-eslint/no-inferrable-types",
+                    ruleArguments: [{ ignoreParameters: true, ignoreProperties: true }],
+                },
+            ],
+        });
+    });
 });
