ee52ef7ff1ccf8f6d22a4da11d34213c88b426b8
Manuel Bojato <30560560+KingDarBoja@users.noreply.github.com>
2020-08-10 18:46:38 -0500

    Add naming-convention merger (#674)

---
+++ src/rules/mergers/tests/naming-convention.test.ts
@@ -0,0 +1,96 @@
+import { mergeNamingConvention } from "../naming-convention";
+
+describe(mergeNamingConvention, () => {
+    test("neither options existing", () => {
+        const result = mergeNamingConvention(undefined, undefined);
+
+        expect(result).toEqual([]);
+    });
+
+    test("original variable name existing", () => {
+        const result = mergeNamingConvention(
+            [
+                {
+                    selector: "variable",
+                    format: ["camelCase", "UPPER_CASE"],
+                    leadingUnderscore: "forbid",
+                    trailingUnderscore: "forbid",
+                },
+            ],
+            undefined,
+        );
+
+        expect(result).toEqual([
+            {
+                selector: "variable",
+                format: ["camelCase", "UPPER_CASE"],
+                leadingUnderscore: "forbid",
+                trailingUnderscore: "forbid",
+            },
+        ]);
+    });
+
+    test("original interface name existing", () => {
+        const result = mergeNamingConvention(undefined, [
+            {
+                selector: "interface",
+                format: ["PascalCase"],
+                custom: {
+                    regex: "^I[A-Z]",
+                    match: false,
+                },
+            },
+        ]);
+
+        expect(result).toEqual([
+            {
+                selector: "interface",
+                format: ["PascalCase"],
+                custom: {
+                    regex: "^I[A-Z]",
+                    match: false,
+                },
+            },
+        ]);
+    });
+
+    test("both interface and variable name existing", () => {
+        const result = mergeNamingConvention(
+            [
+                {
+                    selector: "variable",
+                    format: ["camelCase", "UPPER_CASE"],
+                    leadingUnderscore: "forbid",
+                    trailingUnderscore: "forbid",
+                },
+            ],
+            [
+                {
+                    selector: "interface",
+                    format: ["PascalCase"],
+                    custom: {
+                        regex: "^I[A-Z]",
+                        match: false,
+                    },
+                },
+            ],
+        );
+
+        expect(result).toEqual([
+            {
+                selector: "variable",
+                format: ["camelCase", "UPPER_CASE"],
+                leadingUnderscore: "forbid",
+                trailingUnderscore: "forbid",
+            },
+            {
+                selector: "interface",
+                format: ["PascalCase"],
+                custom: {
+                    regex: "^I[A-Z]",
+                    match: false,
+                },
+            },
+        ]);
+    });
+});
