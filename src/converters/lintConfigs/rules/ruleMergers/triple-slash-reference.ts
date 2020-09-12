22170eb88986f7cd5bdceea98c505d5e40797656
Josh Goldberg <me@joshuakgoldberg.com>
2020-09-12 15:09:47 -0400

    One last name fix for a merger

--- src/converters/lintConfigs/rules/ruleMergers/triple-slash-reference.ts
+++ src/converters/lintConfigs/rules/ruleMergers/triple-slash-reference.ts
@@ -1,4 +1,4 @@
-import { RuleMerger } from "../merger";
+import { RuleMerger } from "../ruleMerger";
 
 export const mergeTripleSlashReference: RuleMerger = (existingOptions, newOptions) => {
     return [existingOptions?.[0] ?? newOptions?.[0]].filter(Boolean);
