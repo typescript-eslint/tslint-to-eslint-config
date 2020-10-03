f95173b54d3fbfcf1f33eb26e1bf53e7542e0914
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-10-03 19:26:21 -0400

    Changed async addPrettierExtensions to synchronous checkPrettierExtension (#731)

--- src/converters/lintConfigs/summarization/prettier/checkPrettierExtension.test.ts
+++ src/converters/lintConfigs/summarization/prettier/checkPrettierExtension.test.ts
@@ -1,57 +1,57 @@
 import { createEmptyConfigConversionResults } from "../../configConversionResults.stubs";
-import { addPrettierExtensions } from "./addPrettierExtensions";
+import { checkPrettierExtension } from "./checkPrettierExtension";
 
 const createStubRuleConversions = (ruleName: string, ruleSeverity: "error" | "off") =>
     new Map([[ruleName, { ruleName, ruleSeverity }]]);
 
-describe("addPrettierExtensions", () => {
-    it("returns false when a matching converted rule is enabled", async () => {
+describe("checkPrettierExtension", () => {
+    it("returns false when a matching converted rule is enabled", () => {
         // Arrange
         const ruleConversionResults = createEmptyConfigConversionResults({
             converted: createStubRuleConversions("max-len", "error"),
         });
 
         // Act
-        const result = await addPrettierExtensions(ruleConversionResults);
+        const result = checkPrettierExtension(ruleConversionResults);
 
         // Assert
         expect(result).toEqual(false);
     });
 
-    it("returns true when a matching converted rule is disabled", async () => {
+    it("returns true when a matching converted rule is disabled", () => {
         // Arrange
         const ruleConversionResults = createEmptyConfigConversionResults({
             converted: createStubRuleConversions("max-len", "off"),
         });
 
         // Act
-        const result = await addPrettierExtensions(ruleConversionResults);
+        const result = checkPrettierExtension(ruleConversionResults);
 
         // Assert
         expect(result).toEqual(true);
     });
 
-    it("returns true when there are no matching converted rules", async () => {
+    it("returns true when there are no matching converted rules", () => {
         // Arrange
         const ruleConversionResults = createEmptyConfigConversionResults({
             converted: createStubRuleConversions("unknown", "error"),
         });
 
         // Act
-        const result = await addPrettierExtensions(ruleConversionResults);
+        const result = checkPrettierExtension(ruleConversionResults);
 
         // Assert
         expect(result).toEqual(true);
     });
 
-    it("returns true when prettier is requested", async () => {
+    it("returns true when prettier is requested", () => {
         // Arrange
         const ruleConversionResults = createEmptyConfigConversionResults({
             converted: createStubRuleConversions("max-len", "off"),
         });
 
         // Act
-        const result = await addPrettierExtensions(ruleConversionResults, true);
+        const result = checkPrettierExtension(ruleConversionResults, true);
 
         // Assert
         expect(result).toEqual(true);
