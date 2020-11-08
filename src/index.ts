3f4d5fc67029162425331af1683712eb06b87b8e
Josh Goldberg <me@joshuakgoldberg.com>
2020-11-08 11:57:31 -0500

    Meh, just export it all

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,5 @@ export { findOriginalConfigurationsStandalone as findOriginalConfigurations } fr
 export { findReportedConfigurationStandalone as findReportedConfiguration } from "./api/findReportedConfigurationStandalone";
 export { formatOutput } from "./converters/lintConfigs/formatting/formatOutput";
 export { joinConfigConversionResults } from "./converters/lintConfigs/joinConfigConversionResults";
-export {
-    ESLintRuleOptions,
-    ESLintRuleOptionsWithArguments,
-    ESLintRuleSeverity,
-    RawESLintRuleSeverity,
-    TSLintRuleOptions,
-    TSLintRuleSeverity,
-} from "./converters/lintConfigs/rules/types";
+export * from "./converters/lintConfigs/rules/types";
 export * from "./types";
