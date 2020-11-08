04d895fa34cdc760561d6bbf650a710026d44b6b
Josh Goldberg <me@joshuakgoldberg.com>
2020-11-08 11:53:04 -0500

    Exported lint rule types for Node APIs

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,12 @@ export { findOriginalConfigurationsStandalone as findOriginalConfigurations } fr
 export { findReportedConfigurationStandalone as findReportedConfiguration } from "./api/findReportedConfigurationStandalone";
 export { formatOutput } from "./converters/lintConfigs/formatting/formatOutput";
 export { joinConfigConversionResults } from "./converters/lintConfigs/joinConfigConversionResults";
+export {
+    ESLintRuleOptions,
+    ESLintRuleOptionsWithArguments,
+    ESLintRuleSeverity,
+    RawESLintRuleSeverity,
+    TSLintRuleOptions,
+    TSLintRuleSeverity,
+} from "./converters/lintConfigs/rules/types";
 export * from "./types";
