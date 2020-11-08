9d55a89d49f353986ede131eb91fab9b3d6120f6
criskrzysiu <criskrzysiu@gmail.com>
2020-11-08 17:35:40 +0100

    Added rxjs-no-internal converter (#806)
    Co-authored-by: Krzysztof Brilla <brilla.krzysztof@gmail.com>
    Co-authored-by: Manuel Bojato <30560560+KingDarBoja@users.noreply.github.com>

--- src/converters/lintConfigs/rules/ruleConverters.ts
+++ src/converters/lintConfigs/rules/ruleConverters.ts
@@ -187,6 +187,7 @@ import { convertJsxWrapMultiline } from "./ruleConverters/eslint-plugin-react/js
 import { convertNoAsyncSubscribe } from "./ruleConverters/eslint-plugin-rxjs/no-async-subscribe";
 import { convertNoIgnoredReplayBuffer } from "./ruleConverters/eslint-plugin-rxjs/no-ignored-replay-buffer";
 import { convertNoIgnoredTakeWhileValue } from "./ruleConverters/eslint-plugin-rxjs/no-ignored-takewhile-value";
+import { convertNoInternal } from "./ruleConverters/eslint-plugin-rxjs/no-internal";
 import { convertNoRedundantNotify } from "./ruleConverters/eslint-plugin-rxjs/no-redundant-notify";
 import { convertNoShareReplay } from "./ruleConverters/eslint-plugin-rxjs/no-sharereplay";
 import { convertNoUnsafeSubjectNext } from "./ruleConverters/eslint-plugin-rxjs/no-unsafe-subject-next";
@@ -379,6 +380,7 @@ export const ruleConverters = new Map([
     ["rxjs-no-async-subscribe", convertNoAsyncSubscribe],
     ["rxjs-no-ignored-replay-buffer", convertNoIgnoredReplayBuffer],
     ["rxjs-no-ignored-takewhile-value", convertNoIgnoredTakeWhileValue],
+    ["rxjs-no-internal", convertNoInternal],
     ["rxjs-no-redundant-notify", convertNoRedundantNotify],
     ["rxjs-no-sharereplay", convertNoShareReplay],
     ["rxjs-no-unsafe-subject-next", convertNoUnsafeSubjectNext],
