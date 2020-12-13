10cac293db95ef6bda297574facbfe4b1af56c88
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-12-12 19:03:55 -0500

    Added converter for jsx-ban-props (#864)
    * Added converter for jsx-ban-props
    
    * Nest inside a forbid member

---
+++ src/converters/lintConfigs/rules/ruleConverters/eslint-plugin-react/tests/jsx-ban-props.test.ts
@@ -0,0 +1,64 @@
+import { convertJsxBanProps } from "../jsx-ban-props";
+
+describe(convertJsxBanProps, () => {
+    test("conversion without arguments", () => {
+        const result = convertJsxBanProps({
+            ruleArguments: [],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleName: "react/forbid-component-props",
+                },
+            ],
+            plugins: ["eslint-plugin-react"],
+        });
+    });
+
+    test("conversion with a single prop", () => {
+        const result = convertJsxBanProps({
+            ruleArguments: [["someProp"]],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [
+                        {
+                            forbid: "someProp",
+                        },
+                    ],
+                    ruleName: "react/forbid-component-props",
+                },
+            ],
+            plugins: ["eslint-plugin-react"],
+        });
+    });
+
+    test("conversion with a message", () => {
+        const result = convertJsxBanProps({
+            ruleArguments: [["someProp"], ["anotherProp", "Optional explanation"]],
+        });
+
+        expect(result).toEqual({
+            rules: [
+                {
+                    ruleArguments: [
+                        {
+                            forbid: "someProp",
+                        },
+                        {
+                            forbid: {
+                                message: "Optional explanation",
+                                propName: "anotherProp",
+                            },
+                        },
+                    ],
+                    ruleName: "react/forbid-component-props",
+                },
+            ],
+            plugins: ["eslint-plugin-react"],
+        });
+    });
+});
