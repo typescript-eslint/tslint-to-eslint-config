900f146b90e59991bd4949939b9c28871934d50b
Jeremy Hough <highvoltage@gmail.com>
2021-02-27 15:07:18 -0500

    Add codelyzer template-use-track-by-function converter

---
+++ src/converters/lintConfigs/rules/ruleConverters/codelyzer/template-use-track-by-function.ts
@@ -0,0 +1,12 @@
+import { RuleConverter } from "../../ruleConverter";
+
+export const convertTemplateUseTrackByFunction: RuleConverter = () => {
+    return {
+        rules: [
+            {
+                ruleName: "@angular-eslint/template/use-track-by-function",
+            },
+        ],
+        plugins: ["@angular-eslint/eslint-plugin-template"],
+    };
+};
