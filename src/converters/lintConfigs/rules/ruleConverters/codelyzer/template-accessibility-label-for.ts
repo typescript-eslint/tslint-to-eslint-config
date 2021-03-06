ef6f8062fe369d402af46d18b5a2c0d5edc75101
KingDarBoja <stevenbojato04@gmail.com>
2021-03-06 17:59:54 -0500

    Fix codelyzer template-accessibility-label-for converter
    Added the missing rule options

--- src/converters/lintConfigs/rules/ruleConverters/codelyzer/template-accessibility-label-for.ts
+++ src/converters/lintConfigs/rules/ruleConverters/codelyzer/template-accessibility-label-for.ts
@@ -1,9 +1,18 @@
 import { RuleConverter } from "../../ruleConverter";
 
-export const convertTemplateAccessibilityLabelFor: RuleConverter = () => {
+export const convertTemplateAccessibilityLabelFor: RuleConverter = (tslintRule) => {
     return {
         rules: [
             {
+                ...(tslintRule.ruleArguments.length !== 0 && {
+                    ruleArguments: [
+                        {
+                            ...(tslintRule.ruleArguments[0]?.controlComponents && { controlComponents: tslintRule.ruleArguments[0]?.controlComponents }),
+                            ...(tslintRule.ruleArguments[0]?.labelAttributes && { labelAttributes: tslintRule.ruleArguments[0]?.labelAttributes }),
+                            ...(tslintRule.ruleArguments[0]?.labelComponents && { labelComponents: tslintRule.ruleArguments[0]?.labelComponents }),
+                        },
+                    ],
+                }),
                 ruleName: "@angular-eslint/template/accessibility-label-for",
             },
         ],
