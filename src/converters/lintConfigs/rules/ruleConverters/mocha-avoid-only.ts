3596868cfcba1f5602d9ab799f23f4b52b2c84bb
Josh Goldberg <josh@codecademy.com>
2021-03-31 09:53:21 -0400

    Fix source to include plugins

--- src/converters/lintConfigs/rules/ruleConverters/mocha-avoid-only.ts
+++ src/converters/lintConfigs/rules/ruleConverters/mocha-avoid-only.ts
@@ -2,6 +2,7 @@ import { RuleConverter } from "../ruleConverter";
 
 export const convertMochaAvoidOnly: RuleConverter = () => {
     return {
+        plugins: ["eslint-plugin-jest"],
         rules: [
             {
                 ruleName: "jest/no-focused-tests",
