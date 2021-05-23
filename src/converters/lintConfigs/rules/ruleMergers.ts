e3e213877f664001619c2059f112adea692de0b7
Adam Reisinger <reisinger.adam@gmail.com>
2021-05-23 15:45:33 +0200

    Add no-use-before-define merger.

--- src/converters/lintConfigs/rules/ruleMergers.ts
+++ src/converters/lintConfigs/rules/ruleMergers.ts
@@ -7,6 +7,7 @@ import { mergeNoEval } from "./ruleMergers/no-eval";
 import { mergeNoMemberDelimiterStyle } from "./ruleMergers/member-delimiter-style";
 import { mergeNoUnnecessaryTypeAssertion } from "./ruleMergers/no-unnecessary-type-assertion";
 import { mergeTripleSlashReference } from "./ruleMergers/triple-slash-reference";
+import { mergeNoUseBeforeDefine } from "./ruleMergers/no-use-before-define";
 
 export const ruleMergers = new Map([
     ["@typescript-eslint/ban-types", mergeBanTypes],
@@ -14,6 +15,7 @@ export const ruleMergers = new Map([
     ["@typescript-eslint/indent", mergeIndent],
     ["@typescript-eslint/member-delimiter-style", mergeNoMemberDelimiterStyle],
     ["@typescript-eslint/naming-convention", mergeNamingConvention],
+    ["@typescript-eslint/no-use-before-define", mergeNoUseBeforeDefine],
     ["@typescript-eslint/no-unnecessary-type-assertion", mergeNoUnnecessaryTypeAssertion],
     ["@typescript-eslint/triple-slash-reference", mergeTripleSlashReference],
     ["no-empty", mergeNoEmpty],
