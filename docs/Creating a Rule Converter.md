aad56b92a349f16444cceb90d947c303348e7cda
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-05-31 20:53:42 -0400

    Added quick utility script for making a new rule (#1089)
    * Added quick utility script for making a new rule
    
    * Simple import sort: will pull into separate branch
    
    * Enabled eslint-plugin-simple-import-sort internally
    
    * Added eslint-plugin-simple-import-sort
    
    Enforces that exports and imports are sorted alphabetically, with a fixer for ESLint's `--fix`.
    
    Precursor to https://github.com/typescript-eslint/tslint-to-eslint-config/pull/1089 to make it easier to insert lines into `ruleConverters.ts` automatically.
    
    * Reset package-lock.json
    
    * Added back simple-import-sort
    
    * Reset .eslintrc.js
    
    * Remove test bed files
    
    * Also add to ruleConverters.ts, alphabetically

---
+++ docs/Creating a Rule Converter.md
@@ -0,0 +1,16 @@
+# Creating a Rule Converter
+
+> If you're not familiar with this project's rule converters work, please read the [Architecture docs](./Architecture/README.md) first.
+
+Adding a new rule converter to `tslint-to-eslint-config` is a relatively straightforward task.
+For your convenience, a starter script is included that sets up the files:
+
+```shell
+node ./script/newConverter --eslint output-name --tslint input-name
+```
+
+If the lint rule includes arguments, add the `--sameArguments` flag above to have starter code generated for that as well.
+
+```shell
+node ./script/newConverter --eslint output-name --tslint input-name --sameArguments
+```
---
+++ script/newConverter/rewriteConvertersMap.js
@@ -0,0 +1,46 @@
+const { promises: fs } = require("fs");
+const { EOL } = require("node:os");
+const path = require("path");
+
+const filePath = "./src/converters/lintConfigs/rules/ruleConverters.ts";
+
+module.exports.rewriteConvertersMap = async ({ args, tslintPascalCase }) => {
+    const lines = (await fs.readFile(filePath)).toString().split(/\r\n|\r|\n/);
+
+    /**
+     * Inserts a new line alphabetically into the file lines.
+     *
+     * @param {string} insertion Line to be added.
+     * @param {number} start Starting point to begin comparing at.
+     * @param {number} end Last line to compare at, and add just after as a fallback.
+     * @param {(line: string) => string} [mapLine] Transforms lines to be sorted.
+     * @remarks In theory this could use binary search, but... why bother?
+     */
+    const insertAlphabetically = (insertion, start, end, mapLine = (line) => line) => {
+        const sorter = mapLine(insertion);
+
+        for (let i = start; i < lines.length; i += 1) {
+            if (mapLine(lines[i]) > sorter) {
+                lines.splice(i, 0, insertion);
+                return;
+            }
+        }
+
+        lines.splice(end, 0, insertion);
+    };
+
+    insertAlphabetically(
+        `import { convert${tslintPascalCase} } from "./ruleConverters/${args.tslint}";`,
+        0,
+        lines.indexOf(""),
+        (line) => line.split(" from ")[1],
+    );
+
+    insertAlphabetically(
+        `    ["${args.tslint}", convert${tslintPascalCase}],`,
+        lines.indexOf("export const ruleConverters = new Map([") + 1,
+        lines.indexOf("]);"),
+    );
+
+    await fs.writeFile(filePath, lines.join(EOL));
+};
