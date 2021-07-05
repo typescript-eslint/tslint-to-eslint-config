4d58086e251d194ee76cd1b59c4e715ac1869823
Josh Goldberg <me@joshuakgoldberg.com>
2021-07-05 10:34:15 -0400

    Filled in tests

--- src/input/findPackagesConfiguration.test.ts
+++ src/input/findPackagesConfiguration.test.ts
@@ -17,7 +17,7 @@ describe("findPackagesConfiguration", () => {
         expect(dependencies.fileSystem.readFile).toHaveBeenLastCalledWith(`./package.json`);
     });
 
-    it("uses te configuration file from the packages command when one is provided", async () => {
+    it("uses the configuration file from the packages command when one is provided", async () => {
         // Arrange
         const dependencies = {
             fileSystem: createStubFileSystem({
@@ -31,6 +31,23 @@ describe("findPackagesConfiguration", () => {
 
         // Assert
         expect(dependencies.fileSystem.readFile).toHaveBeenLastCalledWith(`./custom/package.json`);
+    });
+
+    it("returns an error when readFile returns an error", async () => {
+        // Arrange
+        const error = new Error("Oh no!");
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: error
+            })
+        }
+        const config = "./custom/package.json";
+
+        // Act
+        const result = await findPackagesConfiguration(dependencies, config);
+
+        // Assert
+        expect(result).toBe(error);
     });
 
     it("applies packages defaults when none are provided", async () => {
@@ -50,5 +67,29 @@ describe("findPackagesConfiguration", () => {
             dependencies: {},
             devDependencies: {},
         });
+    });
+
+    it("uses existing package data when it exists", async () => {
+        // Arrange
+        const data = {
+            dependencies: {
+                eslint: "^11.22.33",
+            },
+            devDependencies: {
+                tslint: "^12.34.56"
+            }
+        }
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: JSON.stringify(data)
+            })
+        }
+        const config = "./package.json";
+
+        // Act
+        const result = await findPackagesConfiguration(dependencies, config);
+
+        // Assert
+        expect(result).toEqual(data);
     });
 });
