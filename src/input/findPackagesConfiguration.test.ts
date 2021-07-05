c2275f3d2b7bdec642f31697502998636db3a6d5
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-07-05 10:36:29 -0400

    Fixed package file parsing for Windows (#1151)
    * Fixed package file parsing for Windows
    
    * Filled in tests

--- src/input/findPackagesConfiguration.test.ts
+++ src/input/findPackagesConfiguration.test.ts
@@ -1,33 +1,62 @@
-import { createStubExec } from "../adapters/exec.stubs";
+import { createStubFileSystem } from "../adapters/fileSystem.stub";
 import { findPackagesConfiguration } from "./findPackagesConfiguration";
 
 describe("findPackagesConfiguration", () => {
     it("defaults the configuration file when one isn't provided", async () => {
         // Arrange
-        const dependencies = { exec: createStubExec() };
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: "{}"
+            })
+        }
 
         // Act
         await findPackagesConfiguration(dependencies, undefined);
 
         // Assert
-        expect(dependencies.exec).toHaveBeenLastCalledWith(`cat "./package.json"`);
+        expect(dependencies.fileSystem.readFile).toHaveBeenLastCalledWith(`./package.json`);
     });
 
-    it("includes a configuration file in the packages command when one is provided", async () => {
+    it("uses the configuration file from the packages command when one is provided", async () => {
         // Arrange
-        const dependencies = { exec: createStubExec() };
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: "{}"
+            })
+        }
         const config = "./custom/package.json";
 
         // Act
         await findPackagesConfiguration(dependencies, config);
 
         // Assert
-        expect(dependencies.exec).toHaveBeenLastCalledWith(`cat "./custom/package.json"`);
+        expect(dependencies.fileSystem.readFile).toHaveBeenLastCalledWith(`./custom/package.json`);
+    });
+
+    it("returns an error when readFile returns an error", async () => {
+        // Arrange
+        const error = new Error("Oh no!");
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: error
+            })
+        }
+        const config = "./custom/package.json";
+
+        // Act
+        const result = await findPackagesConfiguration(dependencies, config);
+
+        // Assert
+        expect(result).toBe(error);
     });
 
     it("applies packages defaults when none are provided", async () => {
         // Arrange
-        const dependencies = { exec: createStubExec({ stdout: "{}" }) };
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: "{}"
+            })
+        }
         const config = "./package.json";
 
         // Act
@@ -38,5 +67,29 @@ describe("findPackagesConfiguration", () => {
             dependencies: {},
             devDependencies: {},
         });
+    });
+
+    it("uses existing package data when it exists", async () => {
+        // Arrange
+        const data = {
+            dependencies: {
+                eslint: "^11.22.33",
+            },
+            devDependencies: {
+                tslint: "^12.34.56"
+            }
+        }
+        const dependencies = {
+            fileSystem: createStubFileSystem({
+                data: JSON.stringify(data)
+            })
+        }
+        const config = "./package.json";
+
+        // Act
+        const result = await findPackagesConfiguration(dependencies, config);
+
+        // Assert
+        expect(result).toEqual(data);
     });
 });
