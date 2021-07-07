9fae612cb3ad60687d8701d74142fbf31854dee1
Josh Goldberg <me@joshuakgoldberg.com>
2021-07-07 11:25:05 -0400

    Looks like my Prettier hook is not running locally or in CI

--- src/converters/editorConfigs/converters/convertVSCodeConfig.ts
+++ src/converters/editorConfigs/converters/convertVSCodeConfig.ts
@@ -36,80 +36,116 @@ export const convertVSCodeConfig: EditorConfigConverter = (rawEditorSettings, se
     }
 
     // Since we've found at least one matching setting, we know the source structure is a proper {}
-    const sourceFile = ts.createSourceFile("settings.json", rawEditorSettings, ts.ScriptTarget.Latest, /*setParentNodes*/ true, ts.ScriptKind.JSON);
-    const jsonRoot = (sourceFile.statements[0] as ts.ExpressionStatement).expression as ts.ObjectLiteralExpression;
+    const sourceFile = ts.createSourceFile(
+        "settings.json",
+        rawEditorSettings,
+        ts.ScriptTarget.Latest,
+        /*setParentNodes*/ true,
+        ts.ScriptKind.JSON,
+    );
+    const jsonRoot = (sourceFile.statements[0] as ts.ExpressionStatement)
+        .expression as ts.ObjectLiteralExpression;
 
-    const propertyIndexByName = (properties: ts.NodeArray<ts.ObjectLiteralElementLike>, name: string) =>
-        properties.findIndex(property => property.name && ts.isStringLiteral(property.name) && property.name.text === name);
+    const propertyIndexByName = (
+        properties: ts.NodeArray<ts.ObjectLiteralElementLike>,
+        name: string,
+    ) =>
+        properties.findIndex(
+            (property) =>
+                property.name && ts.isStringLiteral(property.name) && property.name.text === name,
+        );
 
-    const transformer = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
-        const upsertProperties = (node: ts.ObjectLiteralExpression, additions: readonly [string, string, unknown][]) => {
-            const originalProperties = node.properties;
+    const transformer =
+        (context: ts.TransformationContext) =>
+        (rootNode: ts.SourceFile): ts.SourceFile => {
+            const upsertProperties = (
+                node: ts.ObjectLiteralExpression,
+                additions: readonly [string, string, unknown][],
+            ) => {
+                const originalProperties = node.properties;
 
-            for (const [parent, setting, value] of additions) {
-                const createNewChild = (properties?: ts.NodeArray<ts.ObjectLiteralElementLike>) => {
-                    return context.factory.createPropertyAssignment(
-                        `"${parent}"`,
-                        context.factory.createObjectLiteralExpression(
-                            [
-                                ...properties ?? [],
-                                context.factory.createPropertyAssignment(
-                                    `"${setting}"`,
-                                    typeof value === "string"
-                                        ? context.factory.createStringLiteral(value)
-                                        : context.factory.createTrue()
-                                )
-                            ],
-                            true
-                        ),
-                    );
+                for (const [parent, setting, value] of additions) {
+                    const createNewChild = (
+                        properties?: ts.NodeArray<ts.ObjectLiteralElementLike>,
+                    ) => {
+                        return context.factory.createPropertyAssignment(
+                            `"${parent}"`,
+                            context.factory.createObjectLiteralExpression(
+                                [
+                                    ...(properties ?? []),
+                                    context.factory.createPropertyAssignment(
+                                        `"${setting}"`,
+                                        typeof value === "string"
+                                            ? context.factory.createStringLiteral(value)
+                                            : context.factory.createTrue(),
+                                    ),
+                                ],
+                                true,
+                            ),
+                        );
+                    };
+
+                    const existingIndex = propertyIndexByName(originalProperties, parent);
+
+                    if (existingIndex !== -1) {
+                        const existingProperty = originalProperties[existingIndex];
+                        const updatedProperties = [...node.properties];
+
+                        // We know these casts should be safe because we previously found a matching parent object for the property
+                        updatedProperties[existingIndex] = createNewChild(
+                            (
+                                (existingProperty as ts.PropertyAssignment)
+                                    .initializer as ts.ObjectLiteralExpression
+                            ).properties as ts.NodeArray<ts.ObjectLiteralElementLike> | undefined,
+                        );
+                        node = context.factory.createObjectLiteralExpression(
+                            updatedProperties,
+                            true,
+                        );
+                    } else {
+                        node = context.factory.createObjectLiteralExpression(
+                            [...node.properties, createNewChild()],
+                            true,
+                        );
+                    }
                 }
 
-                const existingIndex = propertyIndexByName(originalProperties, parent);
-
-                if (existingIndex !== -1) {
-                    const existingProperty = originalProperties[existingIndex];
-                    const updatedProperties = [...node.properties];
-
-                    // We know these casts should be safe because we previously found a matching parent object for the property
-                    updatedProperties[existingIndex] = createNewChild(((existingProperty as ts.PropertyAssignment).initializer as ts.ObjectLiteralExpression).properties as ts.NodeArray<ts.ObjectLiteralElementLike> | undefined)
-                    node = context.factory.createObjectLiteralExpression(updatedProperties, true);
-                } else {
-                    node = context.factory.createObjectLiteralExpression([...node.properties, createNewChild()], true);
-                }
-            }
-
-            return node;
-        };
-
-        const visit = (node: ts.Node) => {
-            node = ts.visitEachChild(node, visit, context);
-
-            if (node !== jsonRoot) {
                 return node;
-            }
+            };
 
-            const additions: [string, string, unknown][] = [];
+            const visit = (node: ts.Node) => {
+                node = ts.visitEachChild(node, visit, context);
 
-            if (autoFixOnSave !== undefined) {
-                additions.push(["editor.codeActionsOnSave", "eslint.autoFixOnSave", autoFixOnSave]);
-            }
+                if (node !== jsonRoot) {
+                    return node;
+                }
 
-            if (eslintPathMatches !== undefined) {
-                additions.push(["eslint.options", "configFile", settings.config]);
-            }
+                const additions: [string, string, unknown][] = [];
 
-            return upsertProperties(jsonRoot, additions);
+                if (autoFixOnSave !== undefined) {
+                    additions.push([
+                        "editor.codeActionsOnSave",
+                        "eslint.autoFixOnSave",
+                        autoFixOnSave,
+                    ]);
+                }
+
+                if (eslintPathMatches !== undefined) {
+                    additions.push(["eslint.options", "configFile", settings.config]);
+                }
+
+                return upsertProperties(jsonRoot, additions);
+            };
+
+            return ts.visitNode(rootNode, visit);
         };
-
-        return ts.visitNode(rootNode, visit)
-    };
 
     const printer = ts.createPrinter(undefined);
     const result = ts.transform(sourceFile, [transformer]);
-    const contents = printer.printFile(result.transformed[0])
+    const contents = printer
+        .printFile(result.transformed[0])
         .replace(/^\(/giu, "")
-        .replace(/\);(\r\n|\r|\n)*$/giu, "$1")
+        .replace(/\);(\r\n|\r|\n)*$/giu, "$1");
     result.dispose();
 
     return { contents, missing };
