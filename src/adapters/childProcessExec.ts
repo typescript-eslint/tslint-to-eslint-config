2273dcbb912a7b994d68ddf6604fabebe2fb09d5
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-07-07 07:47:44 -0400

    Revert "Ignore parent NODE_OPTIONS in child_process exec (#1152)" (#1161)
    This reverts commit 6f3f99cae0056f5190db4d32310c1cd7d5a47972.

--- src/adapters/childProcessExec.ts
+++ src/adapters/childProcessExec.ts
@@ -1,23 +1,6 @@
 import { exec } from "child_process";
+import { promisify } from "util";
 
 import { Exec } from "./exec";
 
-export const childProcessExec: Exec = async (command: string) => {
-    return await new Promise((resolve, reject) => {
-        exec(
-            command,
-            {
-                env: {
-                    NODE_OPTIONS: "",
-                },
-            },
-            (error, stdout, stderr) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve({ stderr, stdout });
-                }
-            },
-        );
-    });
-};
+export const childProcessExec: Exec = promisify(exec);
