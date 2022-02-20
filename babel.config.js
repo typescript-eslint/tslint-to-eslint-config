3686e552dc1d49de2c4847c3cbe5236e96e73acd
Josh Goldberg <me@joshuakgoldberg.com>
2022-02-20 14:22:18 -0500

    chore: simplified Jest & babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
     presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
-    plugins: [
-        "@babel/plugin-proposal-nullish-coalescing-operator",
-        "@babel/plugin-proposal-optional-chaining",
-    ],
 };
