82fc246cf3d2b272e8a602b277043c43761e519d
Edwin Kofler <edwin@kofler.dev>
2022-02-26 20:17:18 -0800

    test: Fix to use `@jest/globals`

--- src/converters/lintConfigs/rules/ruleConverters/tests/typedef.test.ts
+++ src/converters/lintConfigs/rules/ruleConverters/tests/typedef.test.ts
@@ -1,6 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+
 import { convertTypedef } from "../typedef";
 
-describe(convertTypedef, () => {
+describe("convertTypedef", () => {
     test("conversion without arguments", () => {
         const result = convertTypedef({
             ruleArguments: [],
