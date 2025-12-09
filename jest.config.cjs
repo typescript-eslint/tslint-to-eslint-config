1207c4ec84644adad537b411a32fcb402feeb54a
Josh Goldberg <git@joshuakgoldberg.com>
2025-12-09 10:15:23 -0500

    Remove branch requirement altogether

--- jest.config.cjs
+++ jest.config.cjs
@@ -13,7 +13,6 @@ module.exports = {
     ],
     coverageThreshold: {
         global: {
-            branches: 100,
             functions: 100,
             lines: 100,
             statements: 100,
